01c4a6f7787631fab2a7046b8ebd7c008c58790a
salman1256@yahoo.com <salman1256@yahoo.com>
2022-03-07 11:00:32 +0530

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
