e1c597a20374f793c1c8ef413f86a3c47fe017df
salman1256@yahoo.com <salman1256@yahoo.com>
2022-03-07 11:09:29 +0530

    After Adding Models

---
+++ Models/projectdbContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WebAppProject.Models
+{
+    public partial class projectdbContext : DbContext
+    {
+        public projectdbContext()
+        {
+        }
+
+        public projectdbContext(DbContextOptions<projectdbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Projects> Projects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:projectsam1256.database.windows.net,1433;Initial Catalog=projectdb;User ID=samadmin;Password=S@1256@1256;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Projects>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.TeamName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
